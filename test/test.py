1da755729a89a567a078d2393c2ae9074eed21f8
maxibooksiyi <maxi1372740910@foxmail.com>
2019-01-08 01:07:51 +0800

    1

---
+++ test/test.py
@@ -0,0 +1,13 @@
+#import tensorflow   
+
+
+import tensorflow as tf
+#matrix1 = tf.constant([[3., 3.]])     
+#matrix2 = tf.constant([[2.],[2.]])    
+#product = tf.matmul(matrix1, matrix2) 
+#sess = tf.Session()    
+#做一点更改
+#DSFDSFD
+
+
+
